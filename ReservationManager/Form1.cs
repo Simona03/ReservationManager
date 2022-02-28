8fedc80ab20ca3560b7dd9bfb38d50962d16c955
it.pgee.burgas <it.pgee.burgas@gmail.com>
2022-02-28 10:42:17 +0200

    added button,name

--- ReservationManager/Form1.cs
+++ ReservationManager/Form1.cs
@@ -21,5 +21,10 @@ namespace ReservationManager
         {
             //Simona
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //pecata
+        }
     }
 }
